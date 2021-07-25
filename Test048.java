c4c6dc2d2995c65ad3284b77d34e64c4a83aef29
leedidi <edy9527@gmail.com>
2021-07-25 16:10:13 +0900

    7/25 MainClass

--- Test048.java
+++ Test048.java
@@ -4,7 +4,7 @@
  ==========================================================*/
  
  // ○ 과제
- // 사용자로부터 임의의 두 정수를 입력받아
+ // 사용자로부터 임의의 정수를 입력받아
  // 1부터 입력받은 그 수 까지의
  // 전체 합과, 짝수의 합과, 홀수의 합을
  // 각각 결과값으로 출력하는 프로그램을 구현한다.
@@ -16,10 +16,57 @@
  // >> 1 ~ 270 까지 홀수의 합 : xxxx
  // 계속하려면 아무 키나 누르세요...
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Test048
 {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) throws IOException
+	{	
+		// 주요 변수 선언 및 초기화
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n=1;					// 사용자가 입력할 임의의 정수 n
+		int i=1;					// while문에 사용할 루프 변수
+		int sum, oddsum, evensum;   // 1 ~ 사용자가 입력할 임의의 정수까지 정수, 짝수, 홀수의 각각 합 저장할 변수
+		sum = oddsum = evensum = 0;
+
+		// 연산 및 처리
+		System.out.print("임의의 정수 입력 : ");
+		n = Integer.parseInt(br.readLine());
+
+		while(i<=n)		
+		{	
+			sum += i;       // 1 ~ 270 까지 정수의 합 구하기
+			
+			if(i%2 == 0)	// 1 ~ 270 까지 짝수의 합 구하기
+			evensum += i;
+			else			// 1 ~ 270 까지 홀수의 값 구하기
+			oddsum += i;
+
+			i++;			// i 1씩 증가
+		}
+
+		// 결과 출력
+			System.out.printf(" >> 1 ~ 270 까지 정수의 합 : %d\n", sum);
+			System.out.printf(" >> 1 ~ 270 까지 짝수의 합 : %d\n", evensum);
+			System.out.printf(" >> 1 ~ 270 까지 홀수의 합 : %d\n", oddsum);
 	}
 }
+
+// 실행 결과
+/*
+임의의 정수 입력 : 3
+ >> 1 ~ 270 까지 정수의 합 : 6
+ >> 1 ~ 270 까지 짝수의 합 : 2
+ >> 1 ~ 270 까지 홀수의 합 : 4
+계속하려면 아무 키나 누르십시오 . . .
+*/
+
+/*
+임의의 정수 입력 : 270
+ >> 1 ~ 270 까지 정수의 합 : 36585
+ >> 1 ~ 270 까지 짝수의 합 : 18360
+ >> 1 ~ 270 까지 홀수의 합 : 18225
+계속하려면 아무 키나 누르십시오 . . .
+*/
--- Test049.java
+++ Test049.java
@@ -22,10 +22,55 @@
  // 1 부터 9 까지의 정수만 입력이 가능합니다.
  // 계속하려면 아무 키나 누르세요...
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Test049
 {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) throws IOException
+	{	
+		//주요 변수 선언 및 초기화
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int i = 1;		//	구구단 i 열에 들어갈 사용자 입력 정수
+		int j = 1;		//  구구단 x2, 3, 4 ... x9 까지 들어갈 정수
+		//int result = 1;	//	구구단 결과
+
+		// 연산 및 처리 + 결과 출력
+		System.out.print("원하는 단(1~9) 입력 : ");
+		i = Integer.parseInt(br.readLine());
+
+		if(i>=1 && i<=9)									// 구구단 1~9단까지만 가능하게 제한
+		{
+		while(j<=9)											// x1, 2 ... x 9 까지 구구단 만들 수 있게 j 조건 제한
+		{
+		System.out.printf("%d * %d = %d\n", i, j, (i*j));	// 구구단 생성
+		j++;
+		}
+		}
+		else
+		System.out.println("1 부터 9 까지의 정수만 입력이 가능합니다.");
+
 	}
 }
+
+// 실행 결과
+/*
+원하는 단(1~9) 입력 : 2
+2 * 1 = 2
+2 * 2 = 4
+2 * 3 = 6
+2 * 4 = 8
+2 * 5 = 10
+2 * 6 = 12
+2 * 7 = 14
+2 * 8 = 16
+2 * 9 = 18
+계속하려면 아무 키나 누르십시오 . . .
+*/
+
+/*
+원하는 단(1~9) 입력 : 11
+1 부터 9 까지의 정수만 입력이 가능합니다.
+계속하려면 아무 키나 누르십시오 . . .
+*/
